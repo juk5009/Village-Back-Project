60400d1e0054a57d85777cd49c9c850846f120bf
khh5762 <khh5762@naver.com>
2023-04-25 17:08:38 +0900

    Place Refactor 완료

--- src/main/java/shop/mtcoding/village/model/category/Category.java
+++ src/main/java/shop/mtcoding/village/model/category/Category.java
@@ -2,8 +2,6 @@ package shop.mtcoding.village.model.category;
 
 import lombok.*;
 import org.hibernate.annotations.Comment;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 import shop.mtcoding.village.model.place.Place;
 
 import javax.persistence.*;
