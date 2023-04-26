82c48bc4be7c981a11758d6591cc51aaabb9c82c
clean17 <piw940317@gmail.com>
2023-04-26 16:37:54 +0900

    스크립트 자동 배포

---
+++ src/main/java/shop/mtcoding/village/controller/pay/MyLogger.java
@@ -0,0 +1,15 @@
+// import io.sentry.Sentry;
+
+// public class MyLogger {
+
+//     public static void log(String message) {
+//         try {
+//             // Do something with the message
+//         } catch (Exception e) {
+//             EventBuilder eventBuilder = new EventBuilder();
+//             eventBuilder.withMessage(message);
+//             Sentry.capture(eventBuilder);
+//         }
+//     }
+
+// }
---
+++ src/main/java/shop/mtcoding/village/controller/pay/PayController.java
@@ -0,0 +1,33 @@
+package shop.mtcoding.village.controller.pay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PayController {
+
+    private final Logger logger = LoggerFactory.getLogger(PayController.class);
+
+    @PostMapping("/bootpaytest")
+    public String bootpaytest(@RequestBody String json){
+        try {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            logger.error("Error occurred while processing JSON data+ jonnnnnnnnnnnnnnnnnnnnn\n\n" + json, e);
+        }
+        return "json";
+    }
+
+    @GetMapping("/bootpaytest1")
+    public String bootpaytest1(){
+        return "json";
+    }
+    
+}
+
+
+
