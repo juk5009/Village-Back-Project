9e0810c053cee26179d5ae546b13befb8a8631f1
juk5009 <juk6340@gmail.com>
2023-04-27 18:25:01 +0900

    정렬 기능 완료

--- src/main/java/shop/mtcoding/village/model/search/SearchRepository.java
+++ src/main/java/shop/mtcoding/village/model/search/SearchRepository.java
@@ -16,30 +16,42 @@ public class SearchRepository {
 
     private final EntityManager em;
 
-    private static final String sql = "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, s.keyword, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
-            "FROM search_tb s " +
-            "INNER JOIN place_tb p ON s.place_id = p.id " +
-            "INNER JOIN address_tb a ON p.address_id = a.id " +
-            "LEFT JOIN review_tb r ON p.id = r.place_id " +
-            "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
-            "WHERE p.title LIKE CONCAT('%', :keyword, '%') OR h.hashtag_name LIKE CONCAT('%', :keyword, '%') " +
-            "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, s.keyword, a.sgg_nm, r.star_rating, h.hashtag_name";
+    private static final String sql =
+            "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, s.keyword, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
+                    "FROM search_tb s " +
+                    "INNER JOIN place_tb p ON s.place_id = p.id " +
+                    "INNER JOIN address_tb a ON p.address_id = a.id " +
+                    "LEFT JOIN review_tb r ON p.id = r.place_id " +
+                    "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
+                    "WHERE p.title LIKE CONCAT('%', :keyword, '%') OR h.hashtag_name LIKE CONCAT('%', :keyword, '%') " +
+                    "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, s.keyword, a.sgg_nm, r.star_rating, h.hashtag_name";
 
-    private static final String sqlPriceDesc = "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
-            "FROM place_tb p " +
-            "INNER JOIN address_tb a ON p.address_id = a.id " +
-            "LEFT JOIN review_tb r ON p.id = r.place_id " +
-            "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
-            "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name " +
-            "ORDER BY p.price_per_hour DESC";
+    private static final String sqlPriceDesc =
+            "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
+                    "FROM place_tb p " +
+                    "INNER JOIN address_tb a ON p.address_id = a.id " +
+                    "LEFT JOIN review_tb r ON p.id = r.place_id " +
+                    "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
+                    "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name " +
+                    "ORDER BY p.price_per_hour DESC";
 
-    private static final String sqlPriceAsc = "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
-            "FROM place_tb p " +
-            "INNER JOIN address_tb a ON p.address_id = a.id " +
-            "LEFT JOIN review_tb r ON p.id = r.place_id " +
-            "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
-            "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name " +
-            "ORDER BY p.price_per_hour ASC";
+    private static final String sqlPriceAsc =
+            "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
+                    "FROM place_tb p " +
+                    "INNER JOIN address_tb a ON p.address_id = a.id " +
+                    "LEFT JOIN review_tb r ON p.id = r.place_id " +
+                    "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
+                    "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name " +
+                    "ORDER BY p.price_per_hour ASC";
+
+    private static final String sqlStarRaingHigh =
+            "SELECT p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name, COUNT(r.id) as review_count " +
+                    "FROM place_tb p " +
+                    "INNER JOIN address_tb a ON p.address_id = a.id " +
+                    "LEFT JOIN review_tb r ON p.id = r.place_id " +
+                    "LEFT JOIN hashtag_tb h ON p.id = h.place_id " +
+                    "GROUP BY p.id, p.title, p.max_people, p.max_parking, p.price_per_hour, a.sgg_nm, r.star_rating, h.hashtag_name " +
+                    "ORDER BY r.star_rating DESC";
 
     public List<SearchList> searchPlacesByKeyword(String keyword) {
         Query query = em.createNativeQuery(sql);
@@ -67,5 +79,14 @@ public class SearchRepository {
         List<SearchOrderby> searchPlaceByPriceAsc = result.list(query, SearchOrderby.class);
 
         return searchPlaceByPriceAsc;
+    }
+
+    public List<SearchOrderby> searchPlaceByStarRaingDescending() {
+        Query query = em.createNativeQuery(sqlStarRaingHigh);
+
+        JpaResultMapper result = new JpaResultMapper();
+        List<SearchOrderby> searchPlaceByStarRaingDesc = result.list(query, SearchOrderby.class);
+
+        return searchPlaceByStarRaingDesc;
     }
 }
