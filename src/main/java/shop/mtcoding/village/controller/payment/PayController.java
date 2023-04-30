c213cd1b55c525e443b9a0068c2c64eb3b913b05
clean17 <piw940317@gmail.com>
2023-05-01 01:27:29 +0900

    h2 shutdown hook 비활성화

--- src/main/java/shop/mtcoding/village/controller/payment/PayController.java
+++ src/main/java/shop/mtcoding/village/controller/payment/PayController.java
@@ -2,10 +2,14 @@ package shop.mtcoding.village.controller.payment;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import shop.mtcoding.village.dto.ResponseDTO;
+
 
 @RestController
 public class PayController {
@@ -13,13 +17,13 @@ public class PayController {
     private final Logger logger = LoggerFactory.getLogger(PayController.class);
 
     @PostMapping("/bootpaytest")
-    public String bootpaytest(@RequestBody String json){
+    public ResponseEntity<?> bootpaytest(@RequestBody String json){
         try {
             throw new RuntimeException();
         } catch (Exception e) {
-            logger.error("Error occurred while processing JSON data+ jonnnnnnnnnnnnnnnnnnnnn\n\n" + json, e);
+            logger.error("Error occurred while processing JSON data+ \n\n" + json, e);
         }
-        return "json";
+        return ResponseEntity.ok().body(json);
     }
 
     @GetMapping("/bootpaytest1")
