cd7962f50d8b7a96a83165048d6b2e2918ec2eba
khh5762 <khh5762@naver.com>
2023-05-09 01:37:49 +0900

    fcm 진행중

--- src/main/java/shop/mtcoding/village/api/firebase/FcmController.java
+++ src/main/java/shop/mtcoding/village/api/firebase/FcmController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import shop.mtcoding.village.core.exception.CustomException;
 import shop.mtcoding.village.model.fcm.Fcm;
 import shop.mtcoding.village.model.fcm.FcmRepository;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,14 +42,20 @@ public class FcmController {
 
             Fcm fcm = new Fcm();
             String targetToken = token.split("=")[1];
-            fcm.setTargetToken(targetToken);
-            fcmRepository.save(fcm);
 
+            Optional<Fcm> targetTokenOptional = fcmRepository.findByTargetToken(targetToken);
 
+            if (targetTokenOptional.isEmpty()) {
+                System.out.println("디버그 : 일단 if 문 들어옴");
+                fcm.setTargetToken(targetToken);
+                fcmRepository.save(fcm);
+                System.out.println("디버그 : fcm에 토큰값 : " + fcm);
+
+                // token 받기 성공
+                // token 받고, DB에 저장 후 푸쉬 알림 시 활용
+            }
 
             System.out.println("앱 실행 후 토큰 전송 성공!, token : " + token);
-            // token 받기 성공
-            // token 받고, DB에 저장 후 푸쉬 알림 시 활용
         } else {
             System.out.println("토큰 전송 실패!");
         }
